decc92fe8d8c222451a6cf6bfb91e3a6988c311c
leshiy77 <rifbenya@gmail.com>
2022-05-05 16:40:39 +0300

    feat: add main markup in js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
-import './styles/main.scss'
+import './styles/sass/main.css';
 
+const container = document.createElement('div');
+container.classList.add('container');
+document.body.appendChild(container);
+
+const title = document.createElement('h1');
+title.classList.add('header-title');
+container.appendChild(title);
+title.innerText = 'RSS Virtual Keyboard'
+
+const textArea = document.createElement('textarea');
+textArea.rows = 5;
+textArea.cols = 50;
+textArea.classList.add('textarea');
+container.appendChild(textArea);
+
+const keyboard = document.createElement('div');
+keyboard.classList.add('keyboard');
+container.appendChild(keyboard);
