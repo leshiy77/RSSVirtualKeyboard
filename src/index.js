f91df0e7c79ebb21ff9ddd6f18d060b7b06b0e67
leshiy77 <rifbenya@gmail.com>
2022-05-06 15:05:41 +0300

    feat: add keyboard markup on js and style

--- src/index.js
+++ src/index.js
@@ -14,7 +14,54 @@ textArea.rows = 5;
 textArea.cols = 50;
 textArea.classList.add('textarea');
 container.appendChild(textArea);
+textArea.focus();
 
 const keyboard = document.createElement('div');
 keyboard.classList.add('keyboard');
 container.appendChild(keyboard);
+
+const keyboardRow = document.createElement('div');
+keyboardRow.classList.add('keyboard-row')
+for (let i=0; i<5; i++) {
+    let node = keyboardRow.cloneNode();
+    node.classList.add(`keyboard-row${i+1}`);
+    keyboard.appendChild(node)
+}
+
+const keyBtn = document.createElement('button');
+keyBtn.classList.add('key-button');
+const keyBtnArea = document.querySelectorAll('.keyboard-row');
+keyBtnArea.forEach(element => {
+    switch (element.classList.contains('keyboard-row')) {
+        case element.classList.contains('keyboard-row1'):
+            for (let i =0; i<13;i++) {
+                let nodeBtn = keyBtn.cloneNode();
+                element.appendChild(nodeBtn);
+            }
+        break;
+        case element.classList.contains('keyboard-row2'):
+            for (let i =0; i<14;i++) {
+                let nodeBtn = keyBtn.cloneNode();
+                element.appendChild(nodeBtn);
+            }
+        break;
+        case element.classList.contains('keyboard-row3'):
+            for (let i =0; i<13;i++) {
+                let nodeBtn = keyBtn.cloneNode();
+                element.appendChild(nodeBtn);
+            }
+        break;
+        case element.classList.contains('keyboard-row4'):
+            for (let i =0; i<13;i++) {
+                let nodeBtn = keyBtn.cloneNode();
+                element.appendChild(nodeBtn);
+            }
+        break;
+        case element.classList.contains('keyboard-row5'):
+            for (let i =0; i<8;i++) {
+                let nodeBtn = keyBtn.cloneNode();
+                element.appendChild(nodeBtn);
+            }
+        break;
+    }
+});
---
+++ src/js/Key.js
@@ -0,0 +1,11 @@
+class Key {
+
+    constructor(type, func, name, lang = null) {
+        this.type = type;
+        this.func = func;
+        this.name = name;
+        this.lang = lang;
+    }
+
+
+}
